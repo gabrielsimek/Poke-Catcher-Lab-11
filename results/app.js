5ea9a455dda15afbe8272d7580ce73e71e671b24
Gabriel Simek <gabrielsimek@outlook.com>
2021-04-06 11:06:14 -0700

    add redirect and clear local storage to result page

--- results/app.js
+++ results/app.js
@@ -4,14 +4,22 @@ import { getPokeDex } from '../local-storage-utils.js';
 import { getResults } from './results-utils.js';
 const results = getResults();
 const [caught, encountered, notEncountered, notCaught] = results;
-
+const buttonEl = document.getElementById('play-again')
 
 const caughtEl = document.getElementById('caught');
 caughtEl.textContent = JSON.stringify(caught);
+
 const encounteredEl = document.getElementById('encountered');
+encounteredEl.textContent = JSON.stringify(encountered);
+
 const notEncounteredEl = document.getElementById('not-encountered');
+notEncounteredEl.textContent = JSON.stringify(notEncountered);
+
 const notCaughtEl = document.getElementById('not-caught');
+notCaughtEl.textContent = JSON.stringify(notCaught);
 
 
-
-console.log(caught, encountered, notEncountered, notCaught);
+buttonEl.addEventListener('click', () => {
+    localStorage.removeItem('POKEDEX');
+    window.location = '/';
+});
